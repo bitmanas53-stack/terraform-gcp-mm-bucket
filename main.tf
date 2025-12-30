d92ef46e83b6725bde9309abc7f1f0372c657ab6
Team Thought <manas.malik@worldline.com>
2025-12-30 18:19:56 +0530

    provider added

--- main.tf
+++ main.tf
@@ -1,6 +1,19 @@
-resource "google_storage_bucket" "bucket" {
-  name          = var.bucket_name
-  project       = var.project_id
-  location      = var.region
-  force_destroy = true
+provider "google" {
+  project = var.project_id
 }
+
+module "mm-bucket" {
+  source      = "app.terraform.io/ManasOrgTerraform/mm-bucket/google"
+  version     = "0.0.1"
+  bucket_name = var.bucket_name
+  project_id  = var.project_id
+  region      = var.region
+}
+
+# resource "google_storage_bucket" "bucket" {
+#   name          = var.bucket_name
+#   project       = var.project_id
+#   location      = var.region
+#   force_destroy = true
+# }
+
